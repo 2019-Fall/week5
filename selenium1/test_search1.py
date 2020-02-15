88653ba7f1951ad585415044160cf81aa5dd0874
ahusanov <a.khusanov@gmail.com>
2020-02-15 10:40:26 -0500

    updating selenium file

--- selenium1/test_search1.py
+++ selenium1/test_search1.py
@@ -1,4 +1,4 @@
-## Test Case
+# Test Case
 # 1. Open browser chrome browser
 # 2. Launch the 'automationpractice.com'
 # 3. Enter 't-shirt' in search box
@@ -9,25 +9,23 @@ from selenium import webdriver
 import time
 
 # CONSTANTS
-chrome_url = "C:\\dev\\chromedriver_win32\\chromedriver.exe"
+# chrome_url = "C:\\dev\\chromedriver_win32\\chromedriver.exe"
 home_url = "http://automationpractice.com"
 keyword = 't-shirt'
 
-def test_searching():
-    # step: 1
-    driver = webdriver.Chrome(chrome_url)
-    driver.implicitly_wait(10)
-    driver.maximize_window()
-    # step: 2
-    driver.get(home_url)
-    # step: 3
-    driver.find_element_by_name("search_query").send_keys(keyword)
-    # step: 4
+
+def test_launching_site(browser):
+    browser.get(home_url)
+
+
+def test_entering_the_keyword(browser):
+    browser.find_element_by_name("search_query").send_keys(keyword)
     time.sleep(5)
-    driver.find_element_by_name("submit_search").click()
-    products_list = driver.find_elements_by_xpath("//*[@id='center_column']/ul/li")
-    # step: 5
+    browser.find_element_by_name("submit_search").click()
+
+
+def test_verify_result(browser):
+    products_list = browser.find_elements_by_xpath(
+        "//*[@id='center_column']/ul/li")
     assert len(products_list) >= 1
     time.sleep(5)
-    # step: 6 - closing the browser
-    driver.quit()
